21ce4b73badaddf1673f39ab7b040bbbfc370e46
Sam Lin <slbusi2055@gmail.com>
2025-08-14 23:30:13 -0400

    get all users api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,26 @@ router.get('/get-logged-user', authMiddleware, async (req, res) => {
             success: true,
             data: user,
         });
- 
+
+    } catch (error) {
+        res.send({
+            meesage: error.message,
+            success: false,
+        });
+    }
+});
+
+// GET all users except the currently logged-user
+router.get('/get-all-users', authMiddleware, async (req, res) => {
+    try {
+        const users = await User.find({ _id: { $ne: req.body.userId } });
+
+        res.send({
+            meesage: 'All users fetched successfully.',
+            success: true,
+            data: users,
+        });
+
     } catch (error) {
         res.send({
             meesage: error.message,
