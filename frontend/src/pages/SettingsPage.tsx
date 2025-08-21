39ea3ad9ffc4e778b660264fc59f34e96f93ee16
Sam Lin <slbusi2055@gmail.com>
2025-08-21 01:49:39 -0400

    signup page

---
+++ frontend/src/pages/SettingsPage.tsx
@@ -0,0 +1,10 @@
+
+const SettingsPage = () => {
+  return (
+    <div>
+      SettingsPage
+    </div>
+  )
+}
+
+export default SettingsPage
---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,56 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+interface User {
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string,
+}
+
+interface AuthState {
+  authUser: User | null;
+  isSigningUp: boolean;
+  isLoggingIn: boolean;
+  isUpdatingProfile: boolean;
+  isCheckingAuth: boolean;
+  checkAuth: () => Promise<void>;
+  signup: (data: User) => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+  
+}));
+
